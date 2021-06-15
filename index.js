9d6bbaf2490ab767ab49f9a384adaa23b361bf8b
James Sawley <jsawley@triller.co>
2021-06-15 08:13:28 +0100

    Add some useful console logging

--- index.js
+++ index.js
@@ -25,16 +25,21 @@ const getPRs = async () => {
 }
 
 const reviewers = async () => {
+    console.log('Fetching the latest PRs...')
     const prs = await getPRs();
+
+    console.log(`Fetching reviews on the PRs...`)
     const asyncReviews = prs.map(number => getReviews(number));
     const reviews = await Promise.all(asyncReviews);
 
+    console.log(`Analyzing results...`)
     var reviewers = [];
     reviews.forEach(review => reviewers = reviewers.concat(review) )
 
     const counts = {};
     reviewers.forEach(x => counts[x] = (counts[x] || 0) + 1);
 
+    console.log('')
     console.log(`Here are the reviewers for the last 100 PRs in ${owner}/${repo}:`)
     console.log(counts)
 
